f96f7c27a6b01da4bf2678e19c689d892ed40820
Kerwin <49219656+kerwin-zhao@users.noreply.github.com>
2020-05-29 15:53:50 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- 路由占位符 -->
+    <!-- <route-view></router-view> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
+
+<style>
+</style>
